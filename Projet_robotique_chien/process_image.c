f6b7065a9b7ad884521eff8510c878b5a99be842
Basile77 <basile.pasquale@epfl.ch>
2021-04-15 12:28:46 +0200

    Modif couleur, ajout reconnaissance 3 couleurs

--- Projet_robotique_chien/process_image.c
+++ Projet_robotique_chien/process_image.c
@@ -9,7 +9,7 @@
 #include <process_image.h>
 
 
-static float distance_cm = 0;
+static float distance_cm = 10;
 static uint16_t line_position = IMAGE_BUFFER_SIZE/2;	//middle
 
 //semaphore
@@ -129,7 +129,11 @@ static THD_FUNCTION(ProcessImage, arg) {
     (void)arg;
 
 	uint8_t *img_buff_ptr;
-	uint8_t image[IMAGE_BUFFER_SIZE] = {0};
+
+	uint8_t image_red[IMAGE_BUFFER_SIZE] = {0};
+	uint8_t image_blue[IMAGE_BUFFER_SIZE] = {0};
+	uint8_t image_green[IMAGE_BUFFER_SIZE] = {0};
+
 	uint16_t lineWidth = 0;
 
 	bool send_to_computer = true;
@@ -144,20 +148,23 @@ static THD_FUNCTION(ProcessImage, arg) {
 		for(uint16_t i = 0 ; i < (2 * IMAGE_BUFFER_SIZE) ; i+=2){
 			//extracts first 5bits of the first byte
 			//takes nothing from the second byte
-			image[i/2] = (uint8_t)img_buff_ptr[i]&0xF8;
+			image_red[i/2] = ((uint8_t)img_buff_ptr[i]&0xF8);
+			image_blue[i/2] = (((uint8_t)img_buff_ptr[i+1]&0x1F)<<3);
+			image_green[i/2] = ((((uint8_t)img_buff_ptr[i]&0x03)<<3) + (((uint8_t)img_buff_ptr[i+1]&0xE0)>>5));
+
 		}
 
 		//search for a line in the image and gets its width in pixels
-		lineWidth = extract_line_width(image);
+		//lineWidth = extract_line_width(image);
 
 		//converts the width into a distance between the robot and the camera
 		if(lineWidth){
-			distance_cm = PXTOCM/lineWidth;
+			//distance_cm = PXTOCM/lineWidth;
 		}
 
 		if(send_to_computer){
 			//sends to the computer the image
-			SendUint8ToComputer(image, IMAGE_BUFFER_SIZE);
+			SendUint8ToComputer(image_green, IMAGE_BUFFER_SIZE);
 		}
 		//invert the bool
 		send_to_computer = !send_to_computer;
